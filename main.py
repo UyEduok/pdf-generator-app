cce735a48e3e0876bd4d53f883c0272ac3b2458a
UyEduok <uyeduok@gmail.com>
2023-07-01 21:34:10 +0100

    Added more pages to each title of the pdf file

--- main.py
+++ main.py
@@ -15,4 +15,7 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', style='B', size=12)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='l', ln=1)
 
+    for i in range(row['Pages']-1):
+        pdf.add_page()
+
 pdf.output('output.pdf')
